6b1a60bf97e02506a58ad127460a6541cda12103
sumeet <shirolsumeet01@gmail.com>
2023-09-21 12:25:03 -0700

    1 commit

---
+++ function display().ts
@@ -0,0 +1,12 @@
+function display()
+{
+    var name="sumeet"
+    var age = 18
+    var address = "kle"
+    var mobile = 1234567890
+    console.log("name="+name )
+    console.log("address="+address)
+    console.log("age="+age)
+    console.log("mobile="+mobile)
+}
+display()
---
+++ function display(a,b,c).ts
@@ -0,0 +1,7 @@
+function display(a,b,c){
+    var avg=(a+b+c)/3
+    console.log("avg="+avg);
+}
+
+display(10,20,30)
+
